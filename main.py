fc6606e7703e339ecca1828daa4cb0f4e0cbff16
Haffi Mazhar <haffimazhar96@gmail.com>
2022-10-26 00:23:44 +0100

    movement

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ while run:
     draw_bg()
 
     # Move fighters
-    fighter_1.move()
-    fighter_2.move()
+    fighter_1.move(SCREEN_WIDTH)
+    # fighter_2.move()
 
     # Draw fighters
     fighter_1.draw(screen)
