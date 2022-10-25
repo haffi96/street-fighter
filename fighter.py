c3cab5566669b5c28313e4cc18aa32461052dc53
Haffi Mazhar <haffimazhar96@gmail.com>
2022-10-26 00:13:45 +0100

    init game

---
+++ fighter.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Fighter:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect((x, y, 80, 180))
+
+    def move(self):
+        SPEED = 10
+        dx = 0
+        dy = 0
+
+        # Get user input for keypresses
+        key = pygame.key.get_pressed()
+
+        # Movement keys
+        if key[pygame.K_a]:
+            dx = -SPEED
+        if key[pygame.K_d]:
+            dx = SPEED
+
+        # Update player pos
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame
+
+from fighter import Fighter
+
+pygame.init()
+
+
+# Create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Street fighter")
+
+# Set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+
+# Load background image
+bg_image = pygame.image.load("assets/background.jpg").convert_alpha()
+
+# Add background to pygame
+def draw_bg():
+    scaled_img = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_img, (0, 0))
+
+
+# Create two instances of fighters
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
+
+# Game loop
+run = True
+
+while run:
+    clock.tick(FPS)
+
+    # Draw background
+    draw_bg()
+
+    # Move fighters
+    fighter_1.move()
+    fighter_2.move()
+
+    # Draw fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    # Event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # Update display
+    pygame.display.update()
+
+
+pygame.quit()
